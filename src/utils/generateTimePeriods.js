c939f116f32c9585fde275f0a9c4c3933e14c9b8
gerard1001 <rutayisiregerard2002@gmail.com>
2023-03-26 11:43:28 +0200

    doctor sidebar slots

--- src/utils/generateTimePeriods.js
+++ src/utils/generateTimePeriods.js
@@ -34,3 +34,60 @@ const getTimePeriods = (startingHour, endingHour, duration) => {
 };
 
 export default getTimePeriods;
+
+// const varaibleDate = [
+//   {
+//     appointments: [
+//       {
+//         appointment_period: '10:00 - 11:00',
+//         client: {
+//           client_id: 1,
+//           first_name: 'Amy',
+//           last_name: 'Bond'
+//         }
+//       },
+//       {
+//         appointment_period: '08:00 - 09:00',
+//         client: {
+//           client_id: 1,
+//           first_name: 'Bobby',
+//           last_name: 'Lashley'
+//         }
+//       },
+//       {
+//         appointment_period: '15:00 - 16:00',
+//         client: {
+//           client_id: 1,
+//           first_name: 'Cynthia',
+//           last_name: 'Cole'
+//         }
+//       }
+//     ]
+//   }
+// ];
+
+// const slots = [
+//   '08:00 - 09:00',
+//   '09:00 - 10:00',
+//   '10:00 - 11:00',
+//   '11:00 - 12:00',
+//   '12:00 - 13:00',
+//   '13:00 - 14:00',
+//   '14:00 - 15:00',
+//   '15:00 - 16:00',
+//   '15:00 - 17:00',
+//   '17:00 - 18:00'
+// ];
+
+// result = [
+//   { '08:00 - 09:00': 'Bobby Lashley' },
+//   { '09:00 - 10:00': null },
+//   { '10:00 - 11:00': 'Amy Bond' },
+//   { '11:00 - 12:00': null },
+//   { '12:00 - 13:00': null },
+//   { '13:00 - 14:00': null },
+//   { '14:00 - 15:00': null },
+//   { '15:00 - 16:00': 'Cynthia cole' },
+//   { '15:00 - 17:00': null },
+//   { '17:00 - 18:00': null }
+// ];
