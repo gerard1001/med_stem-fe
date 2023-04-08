f770a9b66fc8452d5203018350e08d538bad2397
Gerard1001 <rutayisiregerard2002@gmail.com>
2023-04-08 17:13:11 +0200

    Graphs --cont

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -6,7 +6,9 @@ import {
   Select,
   Stack,
   styled,
-  Typography
+  Typography,
+  InputLabel,
+  FormControl
 } from '@mui/material';
 import {
   addHours,
@@ -49,7 +51,8 @@ const StyledSelect = styled((props) => (
           borderRadius: '10px',
           borderTopLeftRadius: 0,
           borderTopRightRadius: 0,
-          backgroundColor: '#E7E7E7'
+          backgroundColor: '#E7E7E7',
+          minWidth: '200px'
         }
       }
     }}
@@ -79,13 +82,14 @@ function Analytics() {
   const [showDoctors, setShowDoctors] = useState(false);
   const [showCharts, setShowCharts] = useState(false);
   const selectedDoctorRef = useRef(null);
-  const [selectedDoctor, setSelectedDoctor] = useState('All Doctors');
+  const [step, setStep] = useState(false);
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
   const [selectedPeriodValue, setSelectedPeriodValue] = useState([]);
   const [selectedPeriod, setSelectedPeriod] = useState('This week');
   const [clickedDoctor, setClickedDoctor] = useState(null);
   const selectPeriodRef = useRef();
   const [selectPeriodOpen, setSelectPeriodOpen] = useState(false);
-  const patients = useSelector((state) => state.patient?.data?.data);
+  const allPatients = useSelector((state) => state.patient?.data?.data);
   const allDoctors = useSelector((state) => state.doctor?.data?.data);
   const oneDoctor = useSelector((state) => state.doctor?.single_data?.data);
   const departments = useSelector((state) => state.department?.data?.data);
@@ -96,26 +100,16 @@ function Analytics() {
 
   const toggleToggler = () => {
     setSelectedToggler((value) => !value);
-    setShowDoctors((value) => !value);
+    setShowDoctors(false);
     setShowCharts(true);
+    setSelectedDoctor(null);
+    setClickedDoctor(null);
+    setStep(true);
   };
 
-  const newPatients = useMemo(
-    () =>
-      selectedToggler &&
-      patients?.filter((patient) => {
-        return isBefore(subWeeks(new Date(), 1), new Date(patient?.createdAt));
-      }),
-    [patients, selectedToggler]
-  );
-  const newDoctors = useMemo(
-    () =>
-      !selectedToggler &&
-      allDoctors?.filter((doctor) =>
-        isBefore(subWeeks(new Date(), 1), new Date(doctor?.createdAt))
-      ),
-    [allDoctors, selectedToggler]
-  );
+  useEffect(() => {
+    !isAdmin && setClickedDoctor(loginData?.doctor_id);
+  }, [isAdmin]);
 
   // useEffect(() => {
   //   if (allDoctors) {
@@ -160,14 +154,87 @@ function Analytics() {
 
   const isAdmin = loginData?.Role?.role === 'admin';
 
+  const filteredPatients = allPatients?.filter((patient) => {
+    const hasAppointmentsWithDoctor = patient.appointments.some(
+      (appointment) => appointment.doctor_id === clickedDoctor
+    );
+    return hasAppointmentsWithDoctor;
+  });
+
+  const result = filteredPatients?.map((patient) => {
+    const filteredAppointments = patient.appointments.filter(
+      (appointment) => appointment.doctor_id === clickedDoctor
+    );
+    return {
+      ...patient,
+      appointments: filteredAppointments
+    };
+  });
+
   const doctors = clickedDoctor ? [oneDoctor] : allDoctors;
+  const patients = clickedDoctor ? result : allPatients;
+
+  const newPatients = useMemo(
+    () =>
+      (selectedToggler || clickedDoctor) &&
+      patients?.filter((patient) => {
+        return isBefore(subWeeks(new Date(), 1), new Date(patient?.createdAt));
+      }),
+    [patients, selectedToggler]
+  );
+  const newDoctors = useMemo(
+    () =>
+      (!selectedToggler || showDoctors) &&
+      allDoctors?.filter((doctor) => {
+        return isBefore(subWeeks(new Date(), 1), new Date(doctor?.createdAt));
+      }),
+    [allDoctors, selectedToggler]
+  );
+
+  console.log(patients, 'PATS');
+  console.log(clickedDoctor, 'clickedDoctor');
+  console.log(selectedPeriodValue, '+++');
 
   return (
     <>
       <Box className="w-full max-w-[1200px] h-full mx-auto p-10 sm:p-3">
-        <Typography variant="h6" sx={{ mb: '20px' }} noWrap>
-          Analytics
-        </Typography>
+        <Box className="w-full flex items-center justify-between">
+          <Typography variant="h6" sx={{ mb: '20px' }} noWrap>
+            Analytics
+          </Typography>
+          <Box>
+            {' '}
+            {!isAdmin && (
+              <Box className="max-w-[200px]">
+                <StyledSelect
+                  className="max-w-[200px] w-full"
+                  value={selectedPeriod}
+                  onChange={(e) => {
+                    setSelectedPeriod(e.target.value);
+                  }}
+                >
+                  <MenuItem value="This week">This week</MenuItem>
+                  <MenuItem value="Last month">Last month</MenuItem>
+                  <MenuItem value="Last 3 month">Last 3 month</MenuItem>
+                  <MenuItem value="Last year">Last year</MenuItem>
+                  {selectedPeriod.includes('-') && (
+                    <MenuItem value={selectedPeriod}>{selectedPeriod}</MenuItem>
+                  )}
+                  <MenuItem
+                    ref={selectPeriodRef}
+                    value=""
+                    onClickCapture={(e) => {
+                      e.stopPropagation();
+                      setSelectPeriodOpen(true);
+                    }}
+                  >
+                    Select period
+                  </MenuItem>
+                </StyledSelect>
+              </Box>
+            )}
+          </Box>
+        </Box>
 
         {/* Analytics selectors */}
         {isAdmin && (
@@ -181,50 +248,73 @@ function Analytics() {
               <Typography>Patients</Typography>
               <Box
                 sx={{
-                  backgroundColor: selectedToggler ? '#000' : 'transparent'
+                  backgroundColor:
+                    selectedToggler && !showDoctors ? '#000' : 'transparent'
                 }}
                 className="ml-2 w-[16px] h-[16px] rounded-full border-dark border"
               />
             </Box>
-            {!selectedToggler && showCharts && (
+            {step && (!selectedToggler || showDoctors) && (
               <Box>
                 {!allDoctors || allDoctors.length < 0 ? (
                   <MenuItem value="">No Doctors</MenuItem>
                 ) : (
-                  <StyledSelect
-                    className="max-w-[200px] w-full"
-                    value={selectedDoctor || ''}
-                    onChange={(e) => {
-                      setSelectedDoctor(e.target.value);
-                    }}
-                  >
-                    <MenuItem
-                      value="All Doctors"
-                      onClick={() => {
-                        setClickedDoctor(null);
+                  <FormControl>
+                    <InputLabel
+                      id="demo-multiple-checkbox-label"
+                      sx={{
+                        top: '-7px',
+                        fontWeight: 600,
+                        '& .MuiFormLabel-root': {
+                          top: '-7px',
+                          fontWeight: 600
+                        },
+                        '& .MuiInputLabel-root': {
+                          top: '-7px',
+                          fontWeight: 600
+                        }
                       }}
                     >
-                      All Doctors
-                    </MenuItem>
-                    {allDoctors.map((doctor) => (
+                      Pick a doctor
+                    </InputLabel>
+                    <StyledSelect
+                      className="max-w-[200px] w-full min-w-[200px]"
+                      value={selectedDoctor || ''}
+                      onChange={(e) => {
+                        setSelectedDoctor(e.target.value);
+                      }}
+                    >
                       <MenuItem
-                        key={doctor.doctor_id}
-                        value={doctor.last_name}
+                        value="All Doctors"
                         onClick={() => {
-                          setClickedDoctor(doctor.doctor_id);
-                          dispatch(getOneDoctor(doctor.doctor_id));
+                          setClickedDoctor(null);
+                          setShowDoctors(true);
+                          setSelectedToggler((value) => !value);
                         }}
                       >
-                        {formatDoctorName(doctor)}
+                        All Doctors
                       </MenuItem>
-                    ))}
-                  </StyledSelect>
+                      {allDoctors.map((doctor) => (
+                        <MenuItem
+                          key={doctor.doctor_id}
+                          value={doctor.last_name}
+                          onClick={() => {
+                            setClickedDoctor(doctor.doctor_id);
+                            dispatch(getOneDoctor(doctor.doctor_id));
+                            setSelectedToggler(false);
+                          }}
+                        >
+                          {formatDoctorName(doctor)}
+                        </MenuItem>
+                      ))}
+                    </StyledSelect>
+                  </FormControl>
                 )}
               </Box>
             )}
 
             <StyledSelect
-              className="max-w-[250px] w-full"
+              className="max-w-[200px] w-full"
               value={selectedPeriod}
               onChange={(e) => {
                 setSelectedPeriod(e.target.value);
@@ -252,17 +342,17 @@ function Analytics() {
         )}
 
         {/* Analytics sample data cards */}
-        {showCharts && (
+        {((showCharts && selectedToggler) || clickedDoctor) && (
           <Stack direction="row" className="w-full gap-3 mt-3" flexWrap="wrap">
             <Stack
               direction="column"
               className="p-4 gap-1 bg-[#EDF0F2] w-full max-w-[175px] h-[119px] rounded-xl"
             >
               <Typography className="text-[18px]">
-                New {selectedToggler ? 'Patients' : 'Doctors'}
+                New {!showDoctors || clickedDoctor ? 'Patients' : 'Doctors'}
               </Typography>
               <Typography className="text.dark text-[40px] font-semibold leading-none">
-                {selectedToggler
+                {!showDoctors || clickedDoctor
                   ? newPatients?.length || 0
                   : newDoctors?.length || 0}
               </Typography>
@@ -272,10 +362,10 @@ function Analytics() {
               className="p-4 gap-1 bg-[#EDF0F2] w-full max-w-[175px] h-[119px] rounded-xl"
             >
               <Typography className="text-[18px]">
-                Total {selectedToggler ? 'Patients' : 'Doctors'}
+                Total {!showDoctors || clickedDoctor ? 'Patients' : 'Doctors'}
               </Typography>
               <Typography className="text.dark text-[40px] font-semibold leading-none">
-                {selectedToggler
+                {!showDoctors || clickedDoctor
                   ? patients?.length || 0
                   : allDoctors?.length || 0}
               </Typography>
@@ -283,8 +373,7 @@ function Analytics() {
           </Stack>
         )}
 
-        {/* Analytics Graphs */}
-        {showCharts && (
+        {!isAdmin ? (
           <Box
             sx={{
               display: 'grid',
@@ -298,25 +387,51 @@ function Analytics() {
               pb: 5
             }}
           >
-            {selectedToggler ? (
-              <PatientAnalyticsGraphs
-                isAdmin={isAdmin}
-                patients={patients}
-                doctors={allDoctors}
-                // departments={departments}
-                appointments={appointments}
-                range={selectedPeriodValue}
-              />
-            ) : (
-              <DoctorAnalyticsGraphs
-                clickedDoctor={clickedDoctor}
-                doctors={doctors}
-                departments={departments}
-                appointments={appointments}
-                range={selectedPeriodValue}
-              />
-            )}
+            {' '}
+            <PatientAnalyticsGraphs
+              isAdmin={isAdmin}
+              patients={patients}
+              doctors={allDoctors}
+              appointments={appointments}
+              range={selectedPeriodValue}
+            />
           </Box>
+        ) : (
+          <>
+            {/* Analytics Graphs */}
+            {((showCharts && selectedToggler) || clickedDoctor) && (
+              <Box
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fit, minmax(340px, 1fr))',
+                  gap: 3,
+                  maxWidth: '1900px',
+                  width: '100%',
+                  marginX: 'auto',
+                  height: 'max-content',
+                  mt: 2,
+                  pb: 5
+                }}
+              >
+                {(!showDoctors && selectedToggler) || clickedDoctor !== null ? (
+                  <PatientAnalyticsGraphs
+                    isAdmin={isAdmin}
+                    patients={patients}
+                    doctors={allDoctors}
+                    appointments={appointments}
+                    range={selectedPeriodValue}
+                  />
+                ) : (
+                  <DoctorAnalyticsGraphs
+                    doctors={doctors}
+                    departments={departments}
+                    appointments={appointments}
+                    range={selectedPeriodValue}
+                  />
+                )}
+              </Box>
+            )}
+          </>
         )}
       </Box>
       <SelectDateRangeMenu
